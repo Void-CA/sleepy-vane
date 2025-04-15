7a07943e07e9f9b673257361150ca9b693a05d6c
Void-CA <castilloari282@gmail.com>
2025-04-14 23:46:09 -0600

    fm

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import time
+from datetime import datetime
+
+st.title("Ando con sueño amor")
+st.write("Vamonos a dormir amor")
+# Mostrar la hora actual con Python
+placeholder = st.empty()  # Espacio reservado para la hora
+
+
+# Agregar un GIF
+st.markdown("![Alt Text](https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGszYjFoc2kxYmdtb3QyaXI5Y2x6eWlmZHh6djExNjU3NmpxM2RqNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uk4Va5MkRp2bfkOk6f/giphy.gif)")
+
+while True:
+    current_time = datetime.now().strftime("%H:%M:%S")
+    placeholder.markdown(f"### Que ya son las {current_time}")
+    time.sleep(1)  # Actualiza cada segundo
